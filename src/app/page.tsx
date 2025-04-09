66958dc6a950665e351d1220f5e91ca150147ffd
Oleksandr Myronchuk <smironcuk35@gmail.com>
2025-04-09 10:42:35 +0300

    update tailwind and add config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Header from "./components/common/header";
 import Footer from "./components/common/footer";
 
 export default function Home() {
-  return <Footer />;
-  // <Header />;
+  return (
+    <>
+      <Header />
+      <Footer /> ;
+    </>
+  );
 }

935e2de79e7b210a71da8ed6e0e7adcea64866d5
Oleg-max-tech <olehkostiv2003@gmail.com>
2025-04-12 23:29:51 +0300

    fix-header

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -7,30 +7,27 @@ import Menu from "./Menu";
 import { IconButton } from "../../IconButton/IconButton";
 import ContactInfo from "./ContactInfo";
 import "@/styles/style.css";
-//index
+
 const ICON_CLASS_NAME = "bg-[#e6dcd8] p-3 rounded-md";
 const BUTTON_CLASS_NAME = "text-black w-6 h-6";
 
-export default function Footer() {
-  return (
-    <footer className="bg-dark  max-w-full">
-      <div className="footer-container">
-        <Logo />
-        <div className="flex flex-col md:flex-row md:gap-40 text-center md:text-left justify-between">
-          <Menu />
-          <ContactInfo />
-        </div>
-        <div className="flex flex-col gap-4 self-start">
-          <IconButton buttonClassName={BUTTON_CLASS_NAME}>
-            <FaPhoneAlt className={ICON_CLASS_NAME} />
-          </IconButton>
-
-          <IconButton buttonClassName={BUTTON_CLASS_NAME}>
-            <FaCommentDots className={ICON_CLASS_NAME} />
-          </IconButton>
-        </div>
+export const Footer = () => (
+  <footer className="bg-dark max-w-full">
+    <div className="footer-container">
+      <Logo />
+      <div className="flex flex-col md:flex-row md:gap-40 text-center md:text-left justify-between">
+        <Menu />
+        <ContactInfo />
       </div>
-    </footer>
-  );
-}
-//index
+      <div className="flex flex-col gap-4 self-start">
+        <IconButton buttonClassName={BUTTON_CLASS_NAME}>
+          <FaPhoneAlt className={ICON_CLASS_NAME} />
+        </IconButton>
+
+        <IconButton buttonClassName={BUTTON_CLASS_NAME}>
+          <FaCommentDots className={ICON_CLASS_NAME} />
+        </IconButton>
+      </div>
+    </div>
+  </footer>
+);

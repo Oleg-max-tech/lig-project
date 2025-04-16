5102ba0de6432d9983048f8491a82d754719dc98
Oleg-max-tech <olehkostiv2003@gmail.com>
2025-04-16 16:11:47 +0300

    location

--- src/components/common/Services/index.tsx
+++
@@ -1,9 +0,0 @@
-// import { ServiceSection } from "./ServiceSection";
-
-// export default function ServicesPage() {
-//   return (
-//     <div>
-//       <ServiceSection />
-//     </div>
-//   );
-// }
